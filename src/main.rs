9d06920c91d0cc55ca7db3aa44c7af7b57f54ee3
Julien Graziano <julien@graziano.fr>
2023-02-06 12:38:20 +0100

    refactor: extract logic from main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,33 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        std::process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(err) = run(config) {
+        println!("Application error: {}", err);
+        std::process::exit(1);
+    }
+}
+
+// Logic
+fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("With text:\n{contents}");
+
+    Ok(())
+}
+
+// Config
 struct Config {
     query: String,
     filename: String,
@@ -17,20 +44,4 @@ impl Config {
 
         Ok(Config { query, filename })
     }
-}
-
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
-        std::process::exit(1);
-    });
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
-    let contents =
-        fs::read_to_string(config.filename).expect("Failed to read file");
-
-    println!("With text:\n{contents}");
 }
