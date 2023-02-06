d985eec093d675b70a86049a25433e7b87d43c1f
Julien Graziano <julien@graziano.fr>
2023-02-06 12:46:35 +0100

    refactor: splitting code into a library crate

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,22 @@
 use std::env;
-use std::error::Error;
-use std::fs;
+use std::process;
+
+use minigrep;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
-        std::process::exit(1);
-    });
+    let config =
+        minigrep::Config::build(&args)
+            .unwrap_or_else(|err| {
+                println!("Problem parsing arguments: {}", err);
+                process::exit(1);
+            });
 
     println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
+    println!("In file {}", config.file_path);
 
-    if let Err(err) = run(config) {
+    if let Err(err) = minigrep::run(config) {
         println!("Application error: {}", err);
-        std::process::exit(1);
-    }
-}
-
-// Logic
-fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{contents}");
-
-    Ok(())
-}
-
-// Config
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Self, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
+        process::exit(1);
     }
 }
