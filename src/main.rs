38faed0e02ecf351abf14910eef92bb2bfc62703
Julien Graziano <julien@graziano.fr>
2023-02-06 12:28:46 +0100

    refactor: better error handling

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,24 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Self {
+    fn build(args: &[String]) -> Result<Self, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
-    
-        Config { query, filename }
+
+        Ok(Config { query, filename })
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        std::process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
