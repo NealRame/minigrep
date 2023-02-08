268215765a4fe542179de19e91c9a77dc66e9896
Julien Graziano <julien@graziano.fr>
2023-02-08 23:08:53 +0100

    refactor: enhance command line parsing with iterators

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,11 @@ use std::process;
 use minigrep;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config =
-        minigrep::Config::build(&args)
-            .unwrap_or_else(|err| {
-                eprintln!("Problem parsing arguments: {}", err);
-                process::exit(1);
-            });
+    let config = minigrep::Config::build(env::args())
+        .unwrap_or_else(|err| {
+            eprintln!("Problem parsing arguments: {}", err);
+            process::exit(1);
+        });
 
     if let Err(err) = minigrep::run(config) {
         eprintln!("Application error: {}", err);
